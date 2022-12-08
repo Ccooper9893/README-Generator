7d4ce538762ea590d787dc40bdb106bf4c845a26
Cody Cooper <cody_codster@live.com>
2022-12-07 22:54:58 -0600

    Created prompt questions and updated writeFile function

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,41 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
-const inquirer = require('inquirer');
+const fs = require('fs'); //File System module
+const inquirer = require('inquirer');//Inquirer module (command line interface)
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'title',
+        validate: function (answer) {
+            if (answer.length) {
+                console.log('Please provide a project title!')
+            } else {
+                return true
+            };
+        }
+    },
+    {
+        type: 'input',
+        message: 'Provide a description of your project.',
+        name: 'description',
+        validate: function (answer) {
+            if (answer.length) {
+                console.log('Please provide a description!')
+            } else {
+                return true
+            };
+        }
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('README.md', '', (error) => {
+        error ? console.log(error) : console.log('You have successfully generated a README!');
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
